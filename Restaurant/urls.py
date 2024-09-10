3d013b569c367a4376babcc93a5adb2aed83a43d
Caliphs <caliphs.zvinowanda@gmail.com>
2024-09-10 22:31:29 +0200

    authentification configured

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home', views.index, name='home'),
     path('menu', views.MenuItemView.as_view(), name= 'menu'),
     path('menu/<int:pk>', views.SingleMenuItem.as_view()),
+    path('booking/create', views.BookingView.as_view(), name='booking'),
+    path('api-token-auth', obtain_auth_token)
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -3,12 +3,14 @@ from rest_framework import generics
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -20,4 +22,11 @@ class SingleMenuItem(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BookingView(generics.CreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
     
